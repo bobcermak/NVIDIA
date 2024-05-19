ab0f9141735b6a398d94e58e6939d198570e6756
bobcermak <bohuslavcermak62@gmail.com>
2024-05-19 22:59:18 +0200

    ulozeni

---
+++ main.js
@@ -0,0 +1,47 @@
+
+//Hamburger menu//
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarBurger = document.querySelector('.navbar-burger');
+    const navbarMenu = document.querySelector('.navbar-menu');
+    const navbarClose = document.querySelector('.navbar-close');
+    const navbarBackdrop = document.querySelector('.navbar-backdrop');
+
+    function toggleNavbarMenu() {
+        navbarMenu.classList.toggle('hidden');
+        navbarBackdrop.classList.toggle('hidden');
+        document.body.classList.toggle('overflow-hidden');
+    }
+
+
+    navbarBurger.addEventListener('click', function () {
+        toggleNavbarMenu();
+    });
+
+    navbarClose.addEventListener('click', function () {
+        toggleNavbarMenu();
+    });
+
+  
+    navbarBackdrop.addEventListener('click', function () {
+        toggleNavbarMenu();
+    });
+});
+
+//SLider
+
+var swiper = new Swiper('.swiper-container', {
+
+    loop: true, 
+    slidesPerView: 1, 
+    spaceBetween: 30, 
+    autoplay: {
+        delay: 5000, 
+        disableOnInteraction: false, 
+    },
+    
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
